c3f1b45d96b1d21b3dfeed72749b40ce5a738233
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-04-24 00:44:26 +0800

    error catching with try and except

--- file_handling_separating_odd_even.py
+++ file_handling_separating_odd_even.py
@@ -1,29 +1,34 @@
-#accessing the files and closing after reading 
-file = open("number.txt", "r")
-data = file.read()
-numbers = data.split()
-file.close()
+try:
+    #accessing the files and closing after reading 
+    file = open("numbers.txt", "r")
+    data = file.read()
+    numbers = data.split()
+    file.close()
 
-even = []
-odd = []
+    even = []
+    odd = []
 
 # logic to separate the od and even number using if and else
-for num in numbers:
-    num = int(num)
+    for num in numbers:
+        num = int(num)
     if num % 2 == 0:
         even.append(str(num))
     else:
         odd.append(str(num))
 
+    even_file = open("even.txt", "w")
+    for num in even:
+        even_file.write(num + " ")
+    even_file.close()
 
-even_file = open("even.txt", "w")
-for num in even:
-    even_file.write(num + " ")
-even_file.close()
+    odd_file = open("odd.txt", "w")
+    for num in odd:
+        odd_file.write(num + " ")
+    odd_file.close()
 
-odd_file = open("odd.txt", "w")
-for num in odd:
-    odd_file.write(num + " ")
-odd_file.close()
-
-print("Files created successfully!")
+#error catching
+    print("Files created successfully!")
+except FileNotFoundError:
+    print("Number.txt not found")
+except ValueError:
+    print("Data on file is invalid")
