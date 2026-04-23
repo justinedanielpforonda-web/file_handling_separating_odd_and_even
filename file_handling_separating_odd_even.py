c4ef95c646a8e5cb9f2af71a18f504f87d022987
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-04-23 22:56:24 +0800

    with file handiling and program separate odd an deven number into file

--- file_handling_separating_odd_even.py
+++ file_handling_separating_odd_even.py
@@ -1,12 +1,29 @@
-#variables to be used
-numbers = []
+#accessing the files and closing after reading 
+file = open("number.txt", "r")
+data = file.read()
+numbers = data.split()
+file.close()
 
 even = []
 odd = []
 
 # logic to separate the od and even number using if and else
 for num in numbers:
+    num = int(num)
     if num % 2 == 0:
-        even.append(num)
+        even.append(str(num))
     else:
-        odd.append(num)
+        odd.append(str(num))
+
+
+even_file = open("even.txt", "w")
+for num in even:
+    even_file.write(num + " ")
+even_file.close()
+
+odd_file = open("odd.txt", "w")
+for num in odd:
+    odd_file.write(num + " ")
+odd_file.close()
+
+print("Files created successfully!")
